3ca253d34846735e34f65585f2b32fe1c37ce875
Alexey Zakruzhetskyi <l500etc@gmail.com>
2019-12-01 01:36:58 +0200

    start with contact information block

--- src/components/contact-information/ContactInformation.js
+++ src/components/contact-information/ContactInformation.js
@@ -4,9 +4,28 @@ import './ContactInformation.scss'
 class ContactInformation extends Component {
   render () {
     return (
-      <div>
-        <h2>Contact Information</h2>
-      </div>
+      <section className="contact-information">
+        <h2>Get in touch</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum iure repellat voluptas! Ab architecto,
+          assumenda blanditiis exercitationem facere impedit incidunt maxime nostrum placeat praesentium quas
+          repudiandae rerum, sed tempora, unde?
+        </p>
+
+        <div className="row">
+          <div className="col-lg-7">
+            <form action="">
+              <div className="form-group">
+                <input type="text" placeholder="Name"/>
+                <input type="email" placeholder="Your Mail"/>
+              </div>
+              <textarea placeholder="Type your message"/>
+              <button type="submit">Send message</button>
+            </form>
+          </div>
+          <div className="col-lg-5"></div>
+        </div>
+      </section>
     )
   }
 }
