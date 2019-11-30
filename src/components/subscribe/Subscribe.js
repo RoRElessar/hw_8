abfaec8070e6e95c8d72782a7ed41df13b4964cf
Alexey Zakruzhetskyi <l500etc@gmail.com>
2019-12-01 01:10:30 +0200

    add subscribe block

--- src/components/subscribe/Subscribe.js
+++ src/components/subscribe/Subscribe.js
@@ -4,9 +4,15 @@ import './Subscribe.scss'
 class Subscribe extends Component {
   render () {
     return (
-      <div>
-        <h2>Subscribe</h2>
-      </div>
+      <section className="subscribe-block">
+        <h2>Design tips, tricks, and freebies. Delivered weekly</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+
+        <form action="">
+          <input type="email" placeholder="Email Address..."/>
+          <button type="submit">Subscribe</button>
+        </form>
+      </section>
     )
   }
 }
